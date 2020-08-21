ea1271a99c04a24b5acb053f6cb2b77d891850d8
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-08-22 00:15:50 +0200

    Add fast approximate antialiasing (FXAA) to Viewport
    This backports FXAA from the `master` branch.
    
    See https://github.com/godotengine/godot/pull/42006/files
    Co-authored-by: Clay John <claynjohn@gmail.com>

--- drivers/gles2/rasterizer_storage_gles2.h
+++ drivers/gles2/rasterizer_storage_gles2.h
@@ -1220,6 +1220,8 @@ public:
 		bool used_in_frame;
 		VS::ViewportMSAA msaa;
 
+		bool use_fxaa;
+
 		RID texture;
 
 		bool used_dof_blur_near;
@@ -1239,6 +1241,7 @@ public:
 				height(0),
 				used_in_frame(false),
 				msaa(VS::VIEWPORT_MSAA_DISABLED),
+				use_fxaa(false),
 				used_dof_blur_near(false),
 				mip_maps_allocated(false) {
 			for (int i = 0; i < RENDER_TARGET_FLAG_MAX; ++i) {
@@ -1263,6 +1266,7 @@ public:
 	virtual bool render_target_was_used(RID p_render_target);
 	virtual void render_target_clear_used(RID p_render_target);
 	virtual void render_target_set_msaa(RID p_render_target, VS::ViewportMSAA p_msaa);
+	virtual void render_target_set_use_fxaa(RID p_render_target, bool p_fxaa);
 
 	/* CANVAS SHADOW */
 
