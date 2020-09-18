3ffa7cc4f5e7d56c0f3a0ab19a265248d527e7ac
Martin Capitanio <capnm@capitanio.org>
2020-09-18 09:35:19 +0200

    Setup Godot version

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 3
-minor = 2
+minor = 42
 patch = 4
-status = "beta"
+status = "capnm1"
 module_config = ""
-year = 2020
-website = "https://godotengine.org"
+year = 2042
+website = "http://abcdigi.eu/godot3"
