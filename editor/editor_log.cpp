KleirRampage45/redot-engine

Improvement for the Copy button in the Output Log
Now if no text is selected, pressing the Copy button copies the entire text.

editor/editor_log.cpp
@@ -79,7 +79,15 @@ void EditorLog::_clear_request() {
 }
 
 void EditorLog::_copy_request() {
-	log->selection_copy();
+	String text = log->get_selected_text();
+
+	if (text == "") {
+		text = log->get_text();
+	}
+
+	if (text != "") {
+		DisplayServer::get_singleton()->clipboard_set(text);
+	}
 }
 
 void EditorLog::clear() {

capnm/godot

(cherry picked from commit fb6eb21afc0a98b2b1439268abb0facd53222a43)
* See https://github.com/godotengine/godot/pull/42066/files

editor/editor_log.cpp
@@ -82,8 +82,15 @@ void EditorLog::_clear_request() {
 }
 
 void EditorLog::_copy_request() {
+	String text = log->get_selected_text();
 
-	log->selection_copy();
+	if (text == "") {
+		text = log->get_text();
+	}
+
+	if (text != "") {
+		OS::get_singleton()->set_clipboard(text);
+	}
 }
 
 void EditorLog::clear() {